5be780c75d1b2746f9163fc888344aeae74923a7
Foster Brown <fosterbrown@Fosters-iMac.nyc.rr.com>
2019-03-10 17:15:36 -0400

    Created link to amazon app and website

--- app/src/main/java/com/example/nytimes_best_seller/MainActivity.java
+++ app/src/main/java/com/example/nytimes_best_seller/MainActivity.java
@@ -1,13 +1,18 @@
 package com.example.nytimes_best_seller;
 
+import android.content.Intent;
+import android.net.Uri;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 import android.widget.TextView;
 import android.widget.Toast;
 
 import com.example.nytimes_best_seller.API.Model.ServerResponse;
 import com.example.nytimes_best_seller.API.Service.NewYorkTimesAPI;
 
+import java.net.URI;
 import java.util.List;
 
 import retrofit2.Call;
@@ -18,15 +23,18 @@ import retrofit2.converter.gson.GsonConverterFactory;
 
 public class MainActivity extends AppCompatActivity {
 
+    String amazonProductURL;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
         final TextView amazonUrl = findViewById(R.id.textView);
+        final Button buyBook = findViewById(R.id.button_id);
+        buyBook.setVisibility(View.INVISIBLE);
 
         //Setting up the API call using retrofit
-
         Retrofit.Builder builder = new Retrofit.Builder()
                 .baseUrl("https://api.nytimes.com/svc/books/v3/")
                 .addConverterFactory(GsonConverterFactory.create());
@@ -42,7 +50,16 @@ public class MainActivity extends AppCompatActivity {
             @Override
             public void onResponse(Call<ServerResponse> call, Response<ServerResponse> response) {
                 Toast.makeText(MainActivity.this, "Success", Toast.LENGTH_LONG).show();
-                amazonUrl.setText(response.body().getResults().get(4).getAmazonProductUrl());
+                amazonProductURL = response.body().getResults().get(4).getAmazonProductUrl();
+                amazonUrl.setText(amazonProductURL);
+                buyBook.setVisibility(View.VISIBLE);
+
+                buyBook.setOnClickListener(new View.OnClickListener() {
+                    @Override
+                    public void onClick(View view) {
+                        openProductPage(amazonProductURL);
+                    }
+                });
             }
 
             //No response (invalid URL/call)
@@ -51,6 +68,19 @@ public class MainActivity extends AppCompatActivity {
                 Toast.makeText(MainActivity.this, "fail", Toast.LENGTH_LONG).show();
             }
         });
+
+
     }
+
+    public void openProductPage(String url){
+        Uri webpage = Uri.parse(url);
+        Intent intent = new Intent(Intent.ACTION_VIEW, webpage);
+        if(intent.resolveActivity(getPackageManager()) != null){
+            startActivity(intent);
+        }
+    }
+
+
+
 
 }
