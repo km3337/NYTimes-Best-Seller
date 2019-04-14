021e595a782eb65106220edbac0ce7168f87c586
VLGBC <48290744+VLGBC@users.noreply.github.com>
2019-04-14 18:27:26 -0400

    Add placeholder Amazon buy button

--- app/src/main/java/com/example/nytimes_best_seller/BookDetailsActivity.java
+++ app/src/main/java/com/example/nytimes_best_seller/BookDetailsActivity.java
@@ -7,6 +7,7 @@ import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
 import android.widget.Button;
+import android.widget.ImageButton;
 import android.widget.TextView;
 
 
@@ -17,7 +18,7 @@ public class BookDetailsActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_book_details);
 
-        final Button buyButton = findViewById(R.id.buy_button);
+        final ImageButton buyButton = findViewById(R.id.buy_button);
         buyButton.setOnClickListener(new View.OnClickListener() {
             public void onClick(View v) {
                 openProductPage(v);
