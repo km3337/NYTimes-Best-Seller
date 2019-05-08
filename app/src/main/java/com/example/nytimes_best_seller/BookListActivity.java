b7ae1ac87c3d52fd43908d5750ffb721ca1baa1a
Rami <ramisaban1@gmail.com>
2019-05-08 09:05:39 -0400

    Status before merging to master

--- app/src/main/java/com/example/nytimes_best_seller/BookListActivity.java
+++ app/src/main/java/com/example/nytimes_best_seller/BookListActivity.java
@@ -2,6 +2,7 @@ package com.example.nytimes_best_seller;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.Toast;
 
 public class BookListActivity extends AppCompatActivity {
 
@@ -9,5 +10,6 @@ public class BookListActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_book_list);
+        Toast.makeText(this, getIntent().getStringExtra("list_name"), Toast.LENGTH_SHORT).show();
     }
 }
--- app/src/main/java/com/example/nytimes_best_seller/MainActivity.java
+++ app/src/main/java/com/example/nytimes_best_seller/MainActivity.java
@@ -2,24 +2,11 @@ package com.example.nytimes_best_seller;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.util.Log;
-import android.view.MenuItem;
-import android.view.View;
-import android.widget.AdapterView;
-import android.widget.ArrayAdapter;
-import android.widget.Button;
 import android.widget.ListView;
-import android.widget.PopupMenu;
-import android.widget.TextView;
 import android.widget.Toast;
 
-import com.example.nytimes_best_seller.Book_API.Model.BooksResponse;
-import com.example.nytimes_best_seller.Book_API.Service.BooksAPI;
-import com.example.nytimes_best_seller.Category_API.Model.CategoryDetails;
 import com.example.nytimes_best_seller.Category_API.Model.CategoryResponse;
 import com.example.nytimes_best_seller.Category_API.Service.BookCategoriesAPI;
-
-import java.util.List;
 
 import retrofit2.Call;
 import retrofit2.Callback;
@@ -29,10 +16,6 @@ import retrofit2.converter.gson.GsonConverterFactory;
 
 public class MainActivity extends AppCompatActivity {
 
-    String amazonProductURL;
-    ListView bookListView;
-    Button sort;
-    BookList booklist;
     CategoryList categoryList;
 
     @Override
@@ -40,11 +23,7 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         final ListView bookListView = findViewById(R.id.list_view);
-       // booklist = new BookList ( this, bookListView );
         categoryList = new CategoryList(this, bookListView);
-        sort = findViewById ( R.id.sort );
-
-        //Toast.makeText(MainActivity.this,"Loading Books", Toast.LENGTH_LONG).show();
 
         Retrofit.Builder builder = new Retrofit.Builder()
                 .baseUrl("https://api.nytimes.com/svc/books/v3/")
@@ -57,7 +36,6 @@ public class MainActivity extends AppCompatActivity {
             @Override
             public void onResponse(Call<CategoryResponse> call, Response<CategoryResponse> response) {
                 Toast.makeText(MainActivity.this, "Categories Loaded", Toast.LENGTH_SHORT).show();
-               // List<CategoryDetails> categoryDetailsList = response.body().getResults();
                 categoryList.initializeBookList(response.body());
                 categoryList.setItemListener(MainActivity.this, response.body());
             }
@@ -68,22 +46,5 @@ public class MainActivity extends AppCompatActivity {
             }
         });
 
-    }
-
-    public void showPopup(){
-        PopupMenu popupSortMenu = new PopupMenu(MainActivity.this, sort);
-        popupSortMenu.getMenuInflater().inflate(R.menu.popup_sort_menu,popupSortMenu.getMenu());
-        popupSortMenu.show();
-        popupSortMenu.setOnMenuItemClickListener(new PopupMenu.OnMenuItemClickListener() {
-            @Override
-            public boolean onMenuItemClick(MenuItem menuItem) {
-                Log.wtf("tag", menuItem.toString());
-                switch(menuItem.toString()){
-                    case("By Week-Ascending") : booklist.sort("bwa");break;
-                    case("By Week-Descending") : booklist.sort("bwd");break;
-                }
-                return true;
-            }
-        });
     }
 }
